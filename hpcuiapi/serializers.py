9d8a96e5ff249a2dcb11ac1983063e109bbfe479
Juan Leguizamon <juandlm_7@hotmail.com>
2015-11-20 14:28:32 -0500

    Changes on user serializer

--- hpcuiapi/serializers.py
+++ hpcuiapi/serializers.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'first_name', 'last_name', 'email')
+        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'is_staff')
 
 
 class ProjectSerializer(serializers.ModelSerializer):
