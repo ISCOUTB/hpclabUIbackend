23b0b24e0904318f58fe39a1e43266ee9dfa1ab5
Angel Jimenez Escobar <ajimenez@digitalcaribe.com.co>
2017-09-05 20:15:17 -0500

    Eliminando archivos para instalar laravel

--- hpcuiapi/imports.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import status
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
-from django.http import Http404
-from permissions import IsOwner
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework_jwt.authentication import JSONWebTokenAuthentication
-
